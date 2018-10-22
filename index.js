de0d17e71307c4336ae5938c3c502da0ba1e121a
Darshan Shah <iamdarshanshah@gmail.com>
2018-10-22 14:15:58 +0530

    added one more method

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
 const io = require('socket.io-client');
 class client {
-    
+
     constructor(ip) {
         this.access_token = null;
         this.socket = io(ip);
     }
 
     publishSpec(pathToFile) {
+            if (this.access_token !== null) {
+                var fs = require('fs');
+                var fetch = require('node-fetch');
+                fs.readFile('../../'+pathToFile, "utf8", function (err, data) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log(data);
+                    let applicationName = {
+                        yaml: data,
+                    }
 
-        if (this.access_token !== null) {
-            var fs = require('fs');
-            var fetch = require('node-fetch');
-            fs.readFile(pathToFile, "utf8", function (err, data) {
-                if (err) {
-                    return console.log(err);
-                }
-                console.log(data);
-                let applicationName = {
-                    yaml: data,
-                }
-
-                fetch('http://172.23.238.217:8002/register-yaml', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json, text/plain, */*',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(applicationName)
+                    fetch('http://172.23.238.217:8002/register-yaml', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json, text/plain, */*',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(applicationName)
+                    })
                 })
-            })
 
+            }
         }
-    }
 
     configure(tokenFilePath) {
-        // console.log(tokenFilePath,'-----------path');
-        // console.log(typeof tokenFilePath,'-----------type');
         let config = require('../../'+tokenFilePath);
         this.access_token = config.access_token;
         console.log('token sent');
         this.socket.emit('config', this.access_token);
-        // console.log('exit configure');
-        }
+    }
 
     push(eventType, activity) {
         if (this.access_token !== '') {
@@ -51,6 +47,13 @@ class client {
             // console.log('exit push');
         }
 
+    }
+
+    on(tokenFilePath, callback) {
+        this.configure(tokenFilePath);
+        this.socket.on('act stream', (activity) => {
+            callback(activity);
+        });
     }
 
 }
